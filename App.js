d368999ad8b0a5812df79ca4554bc4fab00dab94
GiorgiZosiashvili <zosiashviligiorgi1@gmail.com>
2022-02-24 18:33:35 +0400

    update

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import MyStack from "./src/navigation/MyStack";
+import { StyleSheet, Text, View } from "react-native";
+
 export default function App() {
   return (
     <View style={styles.container}>
